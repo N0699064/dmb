fba57c596c1cf741b629da2cebe5880efd1db50f
Jeff Conrad <32670731+N0699064@users.noreply.github.com>
2025-06-10 01:18:45 +0100

    Update SocialIconsLight.js

--- components/Header/SocialIconsLight.js
+++ components/Header/SocialIconsLight.js
@@ -1,6 +1,7 @@
 // import { InstagramLogo, FacebookLogo, Moon } from 'phosphor-react';
 import { LogoInstagram, LogoFacebook, LogoYoutube } from '@carbon/icons-react';
 import Link from 'next/link';
+import { SiTiktok } from 'react-icons/si';
 
 export default function SocialIconsLight() {
 	return (
@@ -19,7 +20,8 @@ export default function SocialIconsLight() {
 					target='_blank'
 					className='social-icons__link'>
 					{/* <FacebookLogo size={32} color='black' weight='fill' /> */}
-					<LogoFacebook size={30} />
+					{/* <LogoFacebook size={30} /> */}
+					<SiTiktok size={25} />
 				</Link>
 			</li>
 		</ul>
